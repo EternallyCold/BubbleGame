1e6a00494110abe7d92ef18c819e699fa0e12350
Dazza25 <darrengunn25@gmail.com>
2025-01-29 12:26:01 +0000

    Bubble pop, and reset now work

---
+++ Bubble Game/Assets/Scripts/CollisionDelete.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDelete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Col");
+
+        if (other.gameObject.CompareTag("Bubble"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("BUBBLES");
+        }
+    }
+}
