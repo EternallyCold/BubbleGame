50a679c02bcb13a390c6f459a0bf65de779b00b1
Dazza25 <darrengunn25@gmail.com>
2025-01-29 14:45:38 +0000

    Worked on collision scene change, mute works

---
+++ Bubble Game/Assets/Scripts/CollisionSceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionSceneChange : MonoBehaviour
+{
+    [SerializeField]
+    private string SceneName;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("SceneChange"))
+            SceneManager.LoadScene(SceneName);
+        Debug.Log("I have scene CHANGED!!");
+    }
+}
